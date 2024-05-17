2b2a84d498408b6af11143ec6052a945eebe6ed4
Jax Gauthier <justin@justin-tech.com>
2024-05-16 21:33:22 -0400

    Don't fail on errors

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -60,13 +60,7 @@ provider "doppler" {
 
 # Proxmox (modules: vms)
 provider "proxmox" {
-  # url is the hostname (FQDN if you have one) for the proxmox host you'd like to connect to to issue the commands. my proxmox host is 'prox-1u'. Add /api2/json at the end for the API
   pm_api_url = var.pm_api_url
-  # api token id is in the form of: <username>@pam!<tokenId>
-  #pm_api_token_id = "terraform-prov@pve!terraform-prov"
-  # this is the full secret wrapped in quotes. don't worry, I've already deleted this from my proxmox cluster by the time you read this post
-  #pm_api_token_secret = "9ec8e608-d834-4ce5-91d2-15dd59f9a8c1"
-  # leave tls_insecure set to true unless you have your proxmox SSL certificate situation fully sorted out (if you do, you will know)
   pm_tls_insecure = true
   pm_user         = var.pm_user
   pm_password     = var.pm_password
